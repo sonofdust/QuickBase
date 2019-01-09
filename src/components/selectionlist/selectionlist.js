4fb4894e74286f0c04ef0196a25b25d5e3724144
Nicholas D. Roman <NicholasDRoman@hotmail.com>
2019-01-09 00:51:03 -0600

    Reduced overflow function

--- src/components/selectionlist/selectionlist.js
+++ src/components/selectionlist/selectionlist.js
@@ -2,44 +2,28 @@ import React from "react";
 import "./selectionlist.css";
 
 export class Selectionlist extends React.Component {
-  getOptionValue(value, maxLimit) {
-    let optionValue = null;
-
-    if (value.length < maxLimit) {
-      optionValue = value;
-    } else {
-      const normalText = value.substring(0, maxLimit);
-      const redText = value.substring(maxLimit);
-
-      optionValue = (
-        <span>
-          {normalText} <font color="red">{redText}</font>
-        </span>
-      );
-    }
-
+  getOverflow(value, maxLimit) {
     return (
       <div
         key={value}
         className="row option"
         onClick={this.props.deleteItem.bind(this, value)}
       >
-        {optionValue}
+        <span>
+          {value.substring(0, maxLimit)}
+          <font color="red">{value.substring(maxLimit)}</font>
+        </span>
       </div>
     );
   }
 
   render() {
-    const maxLineLimit = 40;
-
     return (
       <div className="list-wrapper">
         <div className="list-title">
           ------ Click list item to unselect ------
         </div>
-        {this.props.choices.map(choice =>
-          this.getOptionValue(choice, maxLineLimit)
-        )}
+        {this.props.choices.map(choice => this.getOverflow(choice, 49))}
       </div>
     );
   }
