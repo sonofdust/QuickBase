dd2f548f95a115edae9ad3a43549c6945e5e03c1
tumentumurchudur <tumen_23@yahoo.com>
2019-01-08 22:53:03 -0700

    Fixes minor issue

--- src/components/selectionlist/selectionlist.js
+++ src/components/selectionlist/selectionlist.js
@@ -3,28 +3,27 @@ import "./selectionlist.css";
 
 export class Selectionlist extends React.Component {
   getOptionValue(value, maxLimit) {
+    let optionValue = null;
+
     if (value.length < maxLimit) {
-      return (
-        <div
-          className="row option"
-          onClick={this.props.deleteItem.bind(this, value)}
-        >
-          {value}
-        </div>
+      optionValue = value;
+    } else {
+      const normalText = value.substring(0, maxLimit);
+      const redText = value.substring(maxLimit);
+
+      optionValue = (
+        <span>
+          {normalText} <font color="red">{redText}</font>
+        </span>
       );
     }
-
-    const normalText = value.substring(0, maxLimit);
-    const redText = value.substring(maxLimit);
 
     return (
       <div
         className="row option"
         onClick={this.props.deleteItem.bind(this, value)}
       >
-        <span>
-          {normalText} <font color="red">{redText}</font>
-        </span>
+        {optionValue}
       </div>
     );
   }
@@ -34,8 +33,12 @@ export class Selectionlist extends React.Component {
 
     return (
       <div className="list-wrapper">
-        <div className="list-title">------ Click list item to unselect ------</div>
-        {this.props.choices.map(choice => this.getOptionValue(choice, maxLineLimit))}
+        <div className="list-title">
+          ------ Click list item to unselect ------
+        </div>
+        {this.props.choices.map(choice =>
+          this.getOptionValue(choice, maxLineLimit)
+        )}
       </div>
     );
   }
